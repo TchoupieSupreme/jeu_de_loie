17ed843d4ab595f3579dad1f74711a2dd590b95c
TchoupieSupreme <passion.johan@gmail.com>
2023-12-07 08:26:41 +0100

    methode

--- src/main/java/ovh/johanpassion/App.java
+++ src/main/java/ovh/johanpassion/App.java
@@ -4,22 +4,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    
+
     public static void main(String[] args) {
-        Random generateur = new Random();
         int caseActuelle = 0;
         Scanner clavier = new Scanner(System.in);
-        for (int i =0;i<5;i++){           
+        for (int i = 0; i < 5; i++) {
+            int nb = nombreAleatoire();
             clavier.nextLine();
-            int lancer = generateur.nextInt(6)+1;
-            caseActuelle += lancer;
-            System.out.println("Lancer "+(i+1)+" : vous avez faits "+lancer+". Vous êtes sur la case "+caseActuelle+" (encore "+(20-caseActuelle)+")");
+            caseActuelle += nb;
+            System.out.println("Lancer " + (i + 1) + " : vous avez faits " + nb + ". Vous êtes sur la case "
+                    + caseActuelle + " (encore " + (20 - caseActuelle) + ")");
         }
-        if (20-caseActuelle == 0) {
-            System.out.println("Vous avez gagné !");
-        }else{
-            System.out.println("Vous avez perdu !");
+        if (20 - caseActuelle == 0) {
+            win();
+        } else {
+            lose();
         }
+        clavier.close();
     }
-    
+
+    public static int nombreAleatoire() {
+        Random generateur = new Random();
+        int nb = generateur.nextInt(6) + 1;
+        return nb;
+    }
+
+    public static void win(){
+        System.out.println("Vous avez gagné !");
+    }
+
+    public static void lose(){
+        System.out.println("Vous avez perdu !");
+    }
+
 }
